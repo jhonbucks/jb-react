06157cd666bae1bc4be46491d6b8514cb3786919
jhonbucks <jhonbucksarte@gmail.com>
2025-08-21 16:16:34 -0300

    new vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jb-react/', // <- NOME DO REPO
+
   server: {
     allowedHosts: [
       'fe31af33daf8.ngrok-free.app', // coloque aqui o host que o erro mostrou
